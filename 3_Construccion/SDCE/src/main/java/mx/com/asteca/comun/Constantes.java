9e007cb0d5d905278547323fd84a87f4aa82de29
amaro.coria <amaro.coria@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-08-19 14:35:45 +0000

    CRUDs para cubrir la tercer pantalla de R1, faltan vistas y fachadas

--- 3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
@@ -6,6 +6,13 @@ public class Constantes {
 	public static final String ASSEMBLER_CATGRAL="assemblerCatGral";
 	public static final String ASSEMBLER_EQUIPOS="assemblerEquipos";
 	public static final String ASSEMBLER_INSTITUTO="assemblerInstituto";
+	public static final String ASSEMBLER_ESTADOS="assemblerEstados";
+	public static final String ASSEMBLER_MUNICIPIOS="assemblerMunicipios";
+	public static final String ASSEMBLER_ASENTAMIENTOS="assemblerAsentamientos";
+	public static final String ASSEMBLER_DOMICILIO="assemblerDomicilio";
+	public static final String ASSEMBLER_ESTATUS="assemblerEstatus";
+	public static final String ASSEMBLER_FAMILIA="assemblerFamilia";
+	public static final String ASSEMBLER_PERSONA="assemblerPersona";
 	public static final String ERROR_OBTENIENDO_LISTA_CATALOGO="No se pudo obtener la lista, intenta mas tarde";
 	public static final String BEAN_EQUIPO="beanEquipo";
 }
