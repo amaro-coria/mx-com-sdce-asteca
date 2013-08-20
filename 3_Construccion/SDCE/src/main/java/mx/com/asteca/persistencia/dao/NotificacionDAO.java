ac0b53b7762663778a316f935fcd513a8bf152b9
rabelt <rabelt@gmail.com>
2013-08-20 22:46:11 +0000

    catalogo notificaciones

---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/persistencia/dao/NotificacionDAO.java
@@ -0,0 +1,11 @@
+package mx.com.asteca.persistencia.dao;
+
+import mx.com.asteca.persistencia.entidades.Notificaciones;
+/**
+ * 
+ * @author ribarra
+ *
+ */
+public interface NotificacionDAO extends BaseDAO<Notificaciones, Integer>{
+
+}
