f1f06b8072c27df04efbe7c3e943f72d3d871c8f
rabelt <rabelt@gmail.com>
2013-08-20 22:46:38 +0000

    catalogo notificaciones

---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/persistencia/dao/impl/NotificacionDAOImpl.java
@@ -0,0 +1,11 @@
+package mx.com.asteca.persistencia.dao.impl;
+
+import mx.com.asteca.persistencia.dao.NotificacionDAO;
+import mx.com.asteca.persistencia.entidades.Notificaciones;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NotificacionDAOImpl extends BaseDAOImpl<Notificaciones, Integer> implements NotificacionDAO{
+	
+}
