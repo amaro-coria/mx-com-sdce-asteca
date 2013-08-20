536dac0972d09eb44d05e97190ddd5b43ef6dd16
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-08-20 22:41:37 +0000

    catalogo notificaciones

--- trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
@@ -13,6 +13,8 @@ public class Constantes {
 	public static final String ASSEMBLER_ESTATUS="assemblerEstatus";
 	public static final String ASSEMBLER_FAMILIA="assemblerFamilia";
 	public static final String ASSEMBLER_PERSONA="assemblerPersona";
+	public static final String ASSEMBLER_NOTIFICACION="assemblerNotificacion";
 	public static final String ERROR_OBTENIENDO_LISTA_CATALOGO="No se pudo obtener la lista, intenta mas tarde";
 	public static final String BEAN_EQUIPO="beanEquipo";
+	public static final String BEAN_NOTIFICACION="beanNotificacion";
 }
