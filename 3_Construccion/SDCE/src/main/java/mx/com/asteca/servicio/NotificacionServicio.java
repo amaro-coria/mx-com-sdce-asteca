cc176a0f859d4f4cb3bdacb16c9f0415b00b92bf
rabelt <rabelt@gmail.com>
2013-08-20 22:46:53 +0000

    catalogo notificaciones

---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/servicio/NotificacionServicio.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package mx.com.asteca.servicio;
+
+import mx.com.asteca.comun.dto.NotificacionDTO;
+import mx.com.asteca.persistencia.entidades.Equipos;
+import mx.com.asteca.persistencia.entidades.Notificaciones;
+
+/**
+ * @author Rabelt Ibarra Godinez.
+ *
+ */
+public interface NotificacionServicio extends BaseService<NotificacionDTO, Integer, Notificaciones> {
+
+}
