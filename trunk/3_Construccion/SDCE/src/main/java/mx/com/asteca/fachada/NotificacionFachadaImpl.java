6ee22af0c610e1caf755034a104e5753c26ce9ff
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-08-20 22:45:55 +0000

    catalogo notificaciones

---
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/fachada/NotificacionFachadaImpl.java
@@ -0,0 +1,38 @@
+package mx.com.asteca.fachada;
+
+import java.util.List;
+
+import mx.com.asteca.comun.dto.NotificacionDTO;
+import mx.com.asteca.servicio.NotificacionServicio;
+import mx.com.asteca.servicio.ServicioException;
+import mx.com.asteca.servicio.impl.NotificacionServicioImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificacionFachadaImpl implements NotificacionFachada{
+
+	@Autowired
+	private NotificacionServicio notificacionServicio;
+	
+	public Integer saveNotificacion(NotificacionDTO notificacion) throws FachadaException {
+		//TODO validar reglas extras de negocio
+		try {
+			Integer pk = notificacionServicio.save(notificacion);
+			return pk;
+		} catch (ServicioException e) {
+			throw new FachadaException("::Propagando exception de servicio::", e);
+		}
+	}
+
+	public List<NotificacionDTO> getAllNotificacion() throws FachadaException {
+		try {
+			List<NotificacionDTO> listaNotificacion = notificacionServicio.getAll();
+			return listaNotificacion;
+		} catch (ServicioException e) {
+			throw new FachadaException("::Propagando exception de servicio::", e);
+		}
+	}
+
+}
