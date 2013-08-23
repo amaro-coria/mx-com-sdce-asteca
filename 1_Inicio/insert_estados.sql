710c97f0472586220d36025134d5862120765070
amaro.coria <amaro.coria@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-08-23 20:10:37 +0000

    Scripts para cargas de datos

---
+++ 1_Inicio/insert_estados.sql
@@ -0,0 +1,38 @@
+
+
+/*!40000 ALTER TABLE `estados` DISABLE KEYS */;
+INSERT INTO `estados` (`ID_ESTADO`,`NOMBRE`,`ACTIVO`,`CLAVE`) VALUES 
+ (1,'Aguascalientes',NULL,NULL),
+ (2,'Baja California',NULL,NULL),
+ (3,'Baja California Sur',NULL,NULL),
+ (4,'Campeche',NULL,NULL),
+ (5,'Coahuila de Zaragoza',NULL,NULL),
+ (6,'Colima',NULL,NULL),
+ (7,'Chiapas',NULL,NULL),
+ (8,'Chihuahua',NULL,NULL),
+ (9,'Distrito Federal',NULL,NULL),
+ (10,'Durango',NULL,NULL),
+ (11,'Guanajuato',NULL,NULL),
+ (12,'Guerrero',NULL,NULL),
+ (13,'Hidalgo',NULL,NULL),
+ (14,'Jalisco',NULL,NULL),
+ (15,'México',NULL,NULL),
+ (16,'Michoacán de Ocampo',NULL,NULL),
+ (17,'Morelos',NULL,NULL),
+ (18,'Nayarit',NULL,NULL),
+ (19,'Nuevo León',NULL,NULL),
+ (20,'Oaxaca',NULL,NULL),
+ (21,'Puebla',NULL,NULL),
+ (22,'Querétaro',NULL,NULL),
+ (23,'Quintana Roo',NULL,NULL),
+ (24,'San Luis Potosí',NULL,NULL),
+ (25,'Sinaloa',NULL,NULL),
+ (26,'Sonora',NULL,NULL),
+ (27,'Tabasco',NULL,NULL),
+ (28,'Tamaulipas',NULL,NULL),
+ (29,'Tlaxcala',NULL,NULL),
+ (30,'Veracruz de Ignacio de la Llave',NULL,NULL),
+ (31,'Yucatán',NULL,NULL),
+ (32,'Zacatecas',NULL,NULL);
+/*!40000 ALTER TABLE `estados` ENABLE KEYS */;
+
