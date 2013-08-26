c9c02c853fe61b1df5ae3b40ac164fbe4848bfcb
rabelt <rabelt@gmail.com>
2013-08-26 21:19:22 +0000

    catalogo notificaciones

--- 3_Construccion/SDCE/src/main/java/mx/com/asteca/vista/NotificacionControlador.java
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/vista/NotificacionControlador.java
@@ -1,35 +1,53 @@
 package mx.com.asteca.vista;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 
 import mx.com.asteca.comun.Constantes;
 import mx.com.asteca.comun.dto.NotificacionDTO;
 import mx.com.asteca.fachada.FachadaException;
 import mx.com.asteca.fachada.NotificacionFachada;
-import mx.com.asteca.fachada.NotificacionFachadaImpl;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ManagedBean(name = Constantes.BEAN_NOTIFICACION)
 @ViewScoped
-public class NotificacionControlador extends BaseController {
+public class NotificacionControlador extends BaseController{
 
+	private static Logger LOGGER = LoggerFactory.getLogger(NotificacionControlador.class);
+	
 	private List<NotificacionDTO> listaNotificacion;
 
-	@Autowired
+	@ManagedProperty("#{notificacionFachadaImpl}")
 	private NotificacionFachada notificacionFachada;
-
-	private void initListaNotificacion() throws FachadaException {
-		if (listaNotificacion == null || CollectionUtils.isEmpty(listaNotificacion)) {
-			notificacionFachada = new NotificacionFachadaImpl();
-			listaNotificacion = notificacionFachada.getAllNotificacion();
+	
+	private void initListaNotificacion() throws FachadaException{
+		if(CollectionUtils.isEmpty(listaNotificacion)){		
+			LOGGER.debug("LLAMANDO A NOTIFICACION FACHADA:"+notificacionFachada);
+			if(notificacionFachada == null){
+				LOGGER.debug("NOTIFICACION FACHADA NULO");
+			}
+			listaNotificacion = notificacionFachada.getAll();
+			if(listaNotificacion != null){
+				for (NotificacionDTO notificacion : listaNotificacion) {
+					LOGGER.debug("INICIALIZANDO NOTIFICACION: "+notificacion.getMensaje());
+				}
+			}
+			else{
+				listaNotificacion = new ArrayList<NotificacionDTO>();
+			}
+		}else{
+			LOGGER.debug("LISTA YA INICIALIZADA");
+			LOGGER.debug("REGRESANDO UNA LISTA CON ELEMENTOS:"+listaNotificacion.size());
 		}
 	}
-
+	
 	/**
 	 * @return the listaNotificacion
 	 */
@@ -43,11 +61,16 @@ public class NotificacionControlador extends BaseController {
 	}
 
 	/**
-	 * @param listaNotificacion
-	 *            the listaNotificacion to set
+	 * @param listaNotificacion the listaNotificacion to set
 	 */
 	public void setListaNotificacion(List<NotificacionDTO> listaNotificacion) {
 		this.listaNotificacion = listaNotificacion;
 	}
 
+	/**
+	 * @param notificacionFachada the notificacionFachada to set
+	 */
+	public void setNotificacionFachada(NotificacionFachada NotificacionFachada) {
+		this.notificacionFachada = NotificacionFachada;
+	}	
 }
