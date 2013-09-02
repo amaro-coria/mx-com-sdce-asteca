5d88e439ee84d9092c42f4bccb398316d87a0bd6
amaro.coria <amaro.coria@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-09-02 22:54:28 +0000

    se suben constantes

--- trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/comun/Constantes.java
@@ -13,14 +13,19 @@ public class Constantes {
 	public static final String ASSEMBLER_ESTATUS="assemblerEstatus";
 	public static final String ASSEMBLER_FAMILIA="assemblerFamilia";
 	public static final String ASSEMBLER_PERSONA="assemblerPersona";
+	public static final String ASSEMBLER_PAISES="assemblerPaises";
 	public static final String ASSEMBLER_NOTIFICACION="assemblerNotificacion";
 	public static final String ASSEMBLER_NOTIFICACION_ESTADO="assemblerNotificacionEstado";
-	public static final String ASSEMBLER_NOTIFICACION_TIPO="assemblerNotificacionTipo";
-	public static final String ASSEMBLER_TIPO_CLIENTE="assemblerTipoCliente";
+	public static final String ASSEMBLER_NOTIFICACION_TIPO="assemblerNotificacionTipo";	public static final String ASSEMBLER_TIPO_CLIENTE="assemblerTipoCliente";
 	public static final String ERROR_OBTENIENDO_LISTA_CATALOGO="No se pudo obtener el catalogo.";
 	public static final String ERROR_NUEVO_REGISTRO="No se pudo completar el nuevo registro.";
 	public static final String BEAN_EQUIPO="beanEquipo";
 	public static final String BEAN_NOTIFICACION="beanNotificacion";
 	public static final String BEAN_CLIENTES="beanClientes";
-	public static final String BEAN_TREE="beanTree";
+	public static final String BEAN_ESTADOS="beanEstados";
+	public static final String BEAN_MUNICIPIOS="beanMunicipios";
+	public static final String ERROR_NO_HAY_SELECCION="Debes seleccionar al menos un elemento.";
+	public static final String ERROR_UPDATE_REGISTRO="Error guardando.";
+	public static final String ERROR_DELETE_REGISTRO="Error borrando.";
+	public static final String BEAN_TREE="beanTree"
 }
