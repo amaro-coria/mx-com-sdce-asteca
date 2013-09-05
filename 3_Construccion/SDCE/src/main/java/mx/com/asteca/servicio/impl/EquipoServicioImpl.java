ecbe6e6d2b47ca7b325e94395951744daa49747b
rabelt <rabelt@gmail.com>
2013-09-05 05:11:41 +0000

    Modificación a PrimeFaces

---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/servicio/impl/EquipoServicioImpl.java
@@ -0,0 +1,35 @@
+package mx.com.asteca.servicio.impl;
+
+import mx.com.asteca.comun.Constantes;
+import mx.com.asteca.comun.dto.EquipoDTO;
+import mx.com.asteca.persistencia.dao.BaseDAO;
+import mx.com.asteca.persistencia.dao.EquipoDAO;
+import mx.com.asteca.persistencia.entidades.Equipos;
+import mx.com.asteca.servicio.assembler.Assembler;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EquipoServicioImpl extends
+		BaseServiceImpl<EquipoDTO, Integer, Equipos> {
+
+	@Autowired
+	private EquipoDAO equipoDAO;
+//	@Autowired
+//	@Qualifier(Constantes.ASSEMBLER_EQUIPOS)
+//	private Assembler assembler;
+
+	@Override
+	BaseDAO getDAO() {
+		return equipoDAO;
+	}
+
+	@Override
+	Assembler getAssembler() {
+//		return assembler;
+		return null;
+	}
+
+}
---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/vista/MenuControlador.java
@@ -0,0 +1,31 @@
+package mx.com.asteca.vista;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "menuBean")
+@ApplicationScoped
+public class MenuControlador extends BaseController {
+	private String navegacion = "/equipos.xhtml";
+
+	public void viewPantalla2() {
+		System.out.println("PRUEBA");
+		this.navegacion = "/pantalla2.xhtml";
+	}
+
+	public void viewNotificacion() {
+		this.navegacion = "/notificacion.xhtml";
+	}
+
+	public void viewEquipo() {
+		this.navegacion = "/equipos.xhtml";
+	}
+
+	public String getNavegacion() {
+		return navegacion;
+	}
+
+	public void setNavegacion(String navegacion) {
+		this.navegacion = navegacion;
+	}
+}
