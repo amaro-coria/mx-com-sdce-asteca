9691777af844bca924948582a0c9153c17689034
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-09-11 23:31:53 +0000

    catalogo notificacion

--- trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/fachada/impl/NotificacionFachadaImpl.java
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/fachada/impl/NotificacionFachadaImpl.java
@@ -1,9 +1,13 @@
 package mx.com.asteca.fachada.impl;
 
+import java.util.List;
+
 import mx.com.asteca.comun.dto.NotificacionDTO;
+import mx.com.asteca.fachada.FachadaException;
 import mx.com.asteca.fachada.NotificacionFachada;
 import mx.com.asteca.servicio.BaseService;
 import mx.com.asteca.servicio.NotificacionServicio;
+import mx.com.asteca.servicio.ServicioException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -18,5 +22,39 @@ public class NotificacionFachadaImpl extends BaseFachadaImpl<NotificacionDTO, In
 	BaseService getBaseService() {
 		return notificacionServicio;
 	}
+
+	@Override
+	public List<NotificacionDTO> getNotificacionByEstado(Long estado)
+			throws FachadaException {
+		try {
+			List<NotificacionDTO> listaNotificacion = notificacionServicio.findByEstado(estado);
+			return listaNotificacion;
+		} catch (ServicioException e) {
+			throw new FachadaException(e.getMessage(), e);
+		}
+	}
+	
+	@Override
+	public List<NotificacionDTO> getNotificacionByTipo(Long tipo)
+			throws FachadaException {
+		try {
+			List<NotificacionDTO> listaNotificacion = notificacionServicio.findByTipo(tipo);
+			return listaNotificacion;
+		} catch (ServicioException e) {
+			throw new FachadaException(e.getMessage(), e);
+		}
+	}
+	
+	@Override
+	public List<NotificacionDTO> getNotificacionByEstadoAndTipo(Long estado, Long tipo)
+			throws FachadaException {
+		try {
+			List<NotificacionDTO> listaNotificacion = notificacionServicio.findByEstadoAndTipo(estado, tipo);
+			return listaNotificacion;
+		} catch (ServicioException e) {
+			throw new FachadaException(e.getMessage(), e);
+		}
+	}
+	
 
 }
