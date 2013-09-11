8358eb76e88e7f17f30ba4cfd01a03f33d587564
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-09-11 23:31:31 +0000

    catalogo notificacion

--- trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/fachada/NotificacionFachada.java
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/fachada/NotificacionFachada.java
@@ -1,8 +1,16 @@
 package mx.com.asteca.fachada;
 
+import java.util.List;
+
 import mx.com.asteca.comun.dto.NotificacionDTO;
 
 
 public interface NotificacionFachada  extends BaseFachada<NotificacionDTO, Integer>{
+
+	List<NotificacionDTO> getNotificacionByEstado(Long estado) throws FachadaException;
+	
+	List<NotificacionDTO> getNotificacionByTipo(Long tipo) throws FachadaException;
+	
+	List<NotificacionDTO> getNotificacionByEstadoAndTipo(Long estado, Long tipo) throws FachadaException;
 	
 }
