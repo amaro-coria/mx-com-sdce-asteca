2c0039600714adab0f4406149174a4e6b98332b1
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-09-11 23:32:56 +0000

    catalogo notificacion

--- trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/servicio/NotificacionServicio.java
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/servicio/NotificacionServicio.java
@@ -3,8 +3,9 @@
  */
 package mx.com.asteca.servicio;
 
+import java.util.List;
+
 import mx.com.asteca.comun.dto.NotificacionDTO;
-import mx.com.asteca.persistencia.entidades.Equipos;
 import mx.com.asteca.persistencia.entidades.Notificaciones;
 
 /**
@@ -12,5 +13,10 @@ import mx.com.asteca.persistencia.entidades.Notificaciones;
  *
  */
 public interface NotificacionServicio extends BaseService<NotificacionDTO, Integer, Notificaciones> {
-
+	
+	public List<NotificacionDTO> findByEstado(Long estado) throws ServicioException;;
+	
+	public List<NotificacionDTO> findByTipo(Long tipo) throws ServicioException;;
+	
+	public List<NotificacionDTO> findByEstadoAndTipo(Long estado, Long tipo) throws ServicioException;;
 }
