44c7f67fd4410af309fd369343e415b7a216f38a
rabelt <rabelt@gmail.com>
2013-09-26 07:54:19 +0000

    Reportes

---
+++ 3_Construccion/SDCE/src/main/java/mx/com/asteca/vista/ReporteInstructorControlador.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package mx.com.asteca.vista;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import mx.com.asteca.comun.Constantes;
+@ManagedBean(name = Constantes.BEAN_REPORTE_INSTRUCTOR)
+@ViewScoped
+public class ReporteInstructorControlador extends BaseController implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
