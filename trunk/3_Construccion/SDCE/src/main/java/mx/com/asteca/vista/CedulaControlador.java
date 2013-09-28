f6474383f9dced05cdde1b0f6d35d63e6380e5a0
rabelt@gmail.com <rabelt@gmail.com@5636fb3f-8ab9-d387-c9db-df812aaa91c8>
2013-09-28 18:11:33 +0000

    Reportes

---
+++ trunk/3_Construccion/SDCE/src/main/java/mx/com/asteca/vista/CedulaControlador.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package mx.com.asteca.vista;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+
+import mx.com.asteca.comun.Constantes;
+import mx.com.asteca.comun.dto.AlumnoDTO;
+import mx.com.asteca.fachada.AlumnoFachada;
+import mx.com.asteca.fachada.FachadaException;
+
+/**
+ * @author ribarra
+ * 
+ */
+@ManagedBean(name = Constantes.BEAN_ADMINISTRATIVOS)
+@ViewScoped
+public class CedulaControlador extends BaseController implements Serializable {
+	@ManagedProperty("#{alumnoFachadaImpl}")
+	private transient AlumnoFachada fachada;
+	private List<AlumnoDTO> listaItems;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private void reporteAlumnos() {
+		try {
+			setListaItems(fachada.getAlumnosDatosBasicos());
+		} catch (FachadaException e) {
+			this.addErrorMessage("Existio un error al consultar la lista de Alumnos");
+			e.printStackTrace();
+		}
+	}
+
+	public List<AlumnoDTO> getListaItems() {
+		return listaItems;
+	}
+
+	public void setListaItems(List<AlumnoDTO> listaItems) {
+		this.listaItems = listaItems;
+	}
+}
